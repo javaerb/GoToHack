dded7bec53ba0ad8939d44405ae67ed61ae257a4
Erb <Erb@CooperDooper.org>
2020-04-29 22:19:30 -0400

    V2.1: improvements

--- src/main/java/org/cooperdooper/ideaplugin/gotohack/MenuInstaller.java
+++ src/main/java/org/cooperdooper/ideaplugin/gotohack/MenuInstaller.java
@@ -1,27 +1,28 @@
 package org.cooperdooper.ideaplugin.gotohack;
 
+import com.intellij.ide.AppLifecycleListener;
 import com.intellij.openapi.actionSystem.ActionManager;
 import com.intellij.openapi.actionSystem.DefaultActionGroup;
-import com.intellij.openapi.application.ApplicationActivationListener;
-import com.intellij.openapi.wm.IdeFrame;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 /**
  * Pulls Go To menu back to top level.
  */
-public class MenuInstaller implements ApplicationActivationListener {
+public class MenuInstaller implements AppLifecycleListener {
 
     private static final String GOTO_MENU_ID = "EditorPopupMenu.GoTo";
 
     private boolean loaded = false;
 
     @Override
-    public void applicationActivated(@NotNull IdeFrame ideFrame) {
+    public void appFrameCreated(@NotNull List<String> commandLineArgs) {
         if (!loaded) {
             loaded = true; // regardless of outcome
             ActionManager manager = ActionManager.getInstance();
             DefaultActionGroup gotoPopup = (DefaultActionGroup) manager.getAction(GOTO_MENU_ID);
-            if (gotoPopup != null) {
+            if (gotoPopup != null && gotoPopup.isPopup()) {
                 gotoPopup.setPopup(false);
                 gotoPopup.addSeparator();
             }
